c5b117a83678d5f976903fded3d13c5d1281366e
Mauro Vieira Gonçalves <mauro.vieira@valtech.com>
2024-08-23 14:17:06 -0400

    Added ODP Snippet in Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { type PropsWithChildren } from "react"
 import type { Metadata } from "next"
+import Script from 'next/script'
 
 // Components
 import { MoseyBankHeader } from '@/components/header'
@@ -25,6 +26,14 @@ type RootLayoutProps = Readonly<PropsWithChildren<{}>>
 
 export default function RootLayout({ children }: RootLayoutProps) {
     return <html lang="en">
+        <Script id="odp-integration">
+            {`
+                var zaius = window['zaius']||(window['zaius']=[]);zaius.methods=["initialize","onload","customer","entity","event","subscribe","unsubscribe","consent","identify","anonymize","dispatch"];zaius.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);zaius.push(t);return zaius}};(function(){for(var i=0;i<zaius.methods.length;i++){var method=zaius.methods[i];zaius[method]=zaius.factory(method)}var e=document.createElement("script");e.type="text/javascript";e.async=true;e.src=("https:"===document.location.protocol?"https://":"http://")+"d1igp3oop3iho5.cloudfront.net/v2/qsSXbLFmfivSll3O4xqCMg/zaius-min.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
+                
+                // Edits to this script should only be made below this line.
+                zaius.event('pageview');
+            `} 
+        </Script>
         <ThemeProvider value={{ theme: "system" }}>
             <Body className={`${figtree.className} bg-ghost-white text-vulcan dark:bg-vulcan dark:text-ghost-white`}>
                 <div className="flex min-h-screen flex-col justify-between">
